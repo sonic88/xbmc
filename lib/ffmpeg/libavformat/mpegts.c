e0f9ac298f476110fa65dfda2eef9fa0747cb5af
xbmc <fernetmenta@online.de>
2012-11-11 16:17:17 +0100

    ffmpeg: update AVProgram on pmt change, send packet for pmt change

--- lib/ffmpeg/libavformat/mpegts.c
+++ lib/ffmpeg/libavformat/mpegts.c
@@ -183,10 +183,25 @@ typedef struct PESContext {
 
 extern AVInputFormat ff_mpegts_demuxer;
 
+static void clear_avprogram(MpegTSContext *ts, unsigned int programid)
+{
+    AVProgram *prg = NULL;
+    int i;
+    for(i=0; i<ts->stream->nb_programs; i++)
+        if(ts->stream->programs[i]->id == programid){
+            prg = ts->stream->programs[i];
+            break;
+        }
+    if (!prg)
+        return;
+    prg->nb_stream_indexes = 0;
+}
+
 static void clear_program(MpegTSContext *ts, unsigned int programid)
 {
     int i;
 
+    clear_avprogram(ts, programid);
     for(i=0; i<ts->nb_prg; i++)
         if(ts->prg[i].id == programid)
             ts->prg[i].nb_pids = 0;
@@ -194,6 +209,9 @@ static void clear_program(MpegTSContext *ts, unsigned int programid)
 
 static void clear_programs(MpegTSContext *ts)
 {
+    int i;
+    for(i=0; i<ts->nb_prg; i++)
+        clear_avprogram(ts, ts->prg[i].id);
     av_freep(&ts->prg);
     ts->nb_prg=0;
 }
@@ -1540,6 +1558,25 @@ static void pmt_cb(MpegTSFilter *filter, const uint8_t *section, int section_len
             }
         }
         p = desc_list_end;
+    }
+
+    if (ts->pkt) {
+        AVStream *st;
+        /* PID of PAT is 0x00, we use this for data stream id */
+        int idx = ff_find_stream_index(ts->stream, 0x00);
+        if (idx >= 0) {
+            st = ts->stream->streams[idx];
+        } else {
+            st = avformat_new_stream(ts->stream, NULL);
+            st->id = 0x00;
+            st->codec->codec_type = AVMEDIA_TYPE_DATA;
+        }
+        ff_program_add_stream_index(ts->stream, h->id, st->index);
+        av_init_packet(ts->pkt);
+        ts->pkt->data = section;
+        ts->pkt->size = section_len;
+        ts->pkt->stream_index = st->index;
+        ts->stop_parse = 1;
     }
 
  out:
